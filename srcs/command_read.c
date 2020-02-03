0d9cd832707cbd9cb35071db54cbb1b5c5650c3a
Nicolas VERON <nveron@e1r10p18.42.fr>
2020-02-03 15:00:44 +0100

    cd ++

--- srcs/command_read.c
+++ srcs/command_read.c
@@ -12,41 +12,45 @@
 
 #include "minishell.h"
 
-static int	init_env_path(t_env *env)
-{
-	char	*curr_path;
-
-	if (!(curr_path = malloc(sizeof(char) * 2)))
-		return (0);
-	curr_path[0] = '~';
-	curr_path[1] = '\0';
-	env->curr_path = curr_path;
-	return (1);
-}
-
 static void	print_curr_path(t_env *env)
 {
 	ft_putstr_fd(env->curr_path, 1);
-	ft_putchar_fd(' ', 1);
+	ft_putstr_fd(" : ", 1);
 }
 
-char		*command_read(t_env *env)
+void	command_read(t_env *env)
 {
 	char	buff[4096];
-	char	*result;
 	int		bytes_readen;
 
-	if (!(result = malloc(sizeof(char) * 1)))
-		return (NULL);
-	result[0] = '\0';
-	init_env_path(env);
 	print_curr_path(env);
 	while ((bytes_readen = read(0, buff, 4095)) > 0)
 	{
 		buff[bytes_readen] = '\0';
-		printf("bytes %d\n\n", bytes_readen);
+		if (ft_strncmp(buff, "cd", 2) == 0)
+			cd_split(env, buff);
+		//printf("bytes %d || buff : %s\n\n", bytes_readen, buff);		//au lieu de faire un printf, ici faire un trie pour envoyer la commande avec la fonction qui l'execute
 		if (bytes_readen < 4095)
 			print_curr_path(env);
 	}
-	return (result);
+}
+
+void	cd_split(t_env *env, char *buff)
+{
+	char *str;
+	int i;
+	int j;
+
+	i = 3;
+	j = 0;
+	if ((!(str = malloc(ft_strlen(buff) * sizeof(char)))) || (buff[2] != ' '))
+		return;
+	while(buff[i])
+	{
+		str[j] = buff[i];
+		i++;
+		j++;
+	}
+	str[j - 1] = '\0';
+	move(str, 0, env);	
 }
