5a57396cf31a2de0674e8ba22da8a89d4326bd0d
Nico <arrvac@gmail.com>
2020-09-17 15:18:27 +0200

    ok

--- srcs/translator_1.c
+++ srcs/translator_1.c
@@ -33,6 +33,19 @@ void	replace_env_norm2(int *length, t_env_variable *env)
 		(*length) = ft_strlen(env->content) - ft_strlen(env->name);
 }
 
+void	test_fill(char **str, int j)
+{
+	int i;
+
+	i = 0;
+	(*str)[j] = '\0';
+	while (i < j)
+	{
+		(*str)[i] = 'k';
+		i++;
+	}
+}
+
 void	replace_env_name_by_value(char **res, t_env_variable *env,
 		int begin_index, int end_index)
 {
@@ -44,10 +57,10 @@ void	replace_env_name_by_value(char **res, t_env_variable *env,
 	replace_env_norm2(&length, env);
 	if (!(tmp = malloc(sizeof(char) * (ft_strlen(*res) + length + 1))))
 		return ;
-	tmp[ft_strlen(*res) + length] = '\0';
+	test_fill(&tmp, ft_strlen(*res) + length);
 	content = env != NULL ? env->content : NULL;
 	replace_env_norm1(&res, &c1, begin_index, &tmp);
-	while (content && content[c1.j] && tmp && tmp[c1.i])
+	while (content && content[c1.j] && tmp[c1.i])
 	{
 		tmp[c1.i] = content[c1.j];
 		(c1.i)++;
